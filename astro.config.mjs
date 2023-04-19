ed286e69d1a7dbb43735ba53f7f3745ae62a952e
liulang <809721414@qq.com>
2023-04-19 22:43:10 +0800

    feat: adapter deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx()
-  ]
-})
+  integrations: [mdx()],
+  output: 'server',
+  adapter: deno(),
+});
