da4b71aff986299ca9f944b433b654bb58a0778c
liulang <809721414@qq.com>
2023-04-20 22:28:50 +0800

    build: 🔧 CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import deno from '@astrojs/deno';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://tangqin-art.github.io',
-  base: '/tangqin-art',
 });
