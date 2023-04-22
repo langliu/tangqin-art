5c1efeadd3444f50f5ff444f1613f65c1d01f444
liulang <809721414@qq.com>
2023-04-22 17:08:50 +0800

    feat: ✨ 新增洛丽塔

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import deno from '@astrojs/deno';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
